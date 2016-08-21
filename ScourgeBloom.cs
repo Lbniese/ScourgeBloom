0b7fd9598538b361b700f2f1eb1326916b344a61
Lbniese <Lbniese@gmail.com>
2016-08-21 20:37:24 +0200

    Versioning
    *Fixed versioning

--- ScourgeBloom.cs
+++ ScourgeBloom.cs
@@ -58,7 +58,7 @@ namespace ScourgeBloom
 
         protected static readonly LocalPlayer Me = StyxWoW.Me;
 
-        public static readonly Version Version = new Version(1, 4, 43);
+        public static readonly Version Version = new Version(1, 4, 44);
 
         private static bool _initialized;
 
